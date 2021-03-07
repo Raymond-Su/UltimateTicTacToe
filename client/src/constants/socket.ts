49150bab0d331f56def7277263fcfaebe06582ab
Raymond Su <rayonixs@gmail.com>
2021-03-07 15:41:03 +1100

    feat: Added socket client

---
+++ client/src/constants/socket.ts
@@ -0,0 +1,1 @@
+export const SOCKET_HOST = 'localhost:5000';
---
+++ client/src/types/socket.ts
@@ -0,0 +1,9 @@
+export enum GameSocketClientMessage {
+  MAKE_MOVE = 'MAKE_MOVE'
+}
+
+export enum GameSocketServerMessage {
+  MOVE_MADE = 'MOVE_MADE',
+  SET_PLAYER = 'SET_PLAYER',
+  CURRENT_BOARD = 'CURRENT_BOARD'
+}
