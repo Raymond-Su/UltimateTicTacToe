911a93e6f3781109a52d30a3ea64b0c28ea05a60
Raymond Su <rayonixs@gmail.com>
2021-03-07 17:11:22 +1100

    fix: removed .env

--- index.ts
+++ index.ts
@@ -2,9 +2,5 @@ import path from 'path';
 import dotenv from 'dotenv';
 import { startServer } from './server';
 
-dotenv.config({
-  path: path.join(__dirname, '../.env')
-});
-
 const apiPort = process.env.PORT || 5000;
 startServer(apiPort);
